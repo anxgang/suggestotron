f4aa4123958470c449c575fb39aec0f190237fe4
ANX <anxgang@gmail.com>
2016-05-04 13:59:39 +0800

    讓大家可以投票

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :topics
-  # The priority is based upon order of creation: first created -> highest priority.
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
